715a20e5d790802376a6e704ba3cc39456d7211a
Joseph Blodgett <Joseph.blodgett@gmail.com>
2018-05-21 10:01:52 -0600

    adding get log status

--- server.go
+++ server.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/byuoitav/authmiddleware"
 	"github.com/byuoitav/common/log"
 	"github.com/byuoitav/couch-db-repl/replication"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	err := replication.Start()
-	if err != nil {
-		log.L.Error(err)
 
+	//start replication
+	go func() {
+		err := replication.Start()
+		if err != nil {
+			log.L.Fatal(err)
+		}
+	}()
+
+	port := ":7012"
+	router := echo.New()
+
+	router.Pre(middleware.RemoveTrailingSlash())
+	router.Use(middleware.CORS())
+
+	// Use the `secure` routing group to require authentication
+	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
+
+	secure.PUT("/log-level/:level", log.SetLogLevel)
+	secure.GET("/log-level", log.GetLogLevel)
+
+	server := http.Server{
+		Addr:           port,
+		MaxHeaderBytes: 1024 * 10,
 	}
+
+	router.StartServer(&server)
 }
